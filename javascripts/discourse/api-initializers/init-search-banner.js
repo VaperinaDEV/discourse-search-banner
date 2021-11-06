14a97cb420160bc427a24322c68ef4d0a8eb1308
Don <71207900+VaperinaDEV@users.noreply.github.com>
2021-11-06 11:01:10 +0100

    Update init-search-banner.js

--- javascripts/discourse/api-initializers/init-search-banner.js
+++ javascripts/discourse/api-initializers/init-search-banner.js
@@ -3,9 +3,7 @@ import { apiInitializer } from "discourse/lib/api";
 export default apiInitializer("0.8", (api) => {
   const enableConnectorName = settings.plugin_outlet;
   const disableConnectorName =
-    enableConnectorName === "above-main-container"
-      ? "below-site-header"
-      : "above-main-container";
+    enableConnectorName === "discovery-list-container-top";
 
   api.registerConnectorClass(disableConnectorName, "search-banner", {
     shouldRender() {
